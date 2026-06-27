b7c85e994e86b90992a20caccc70d0c24064a6f2
GANGOUE MBIMI jihrel Gédéon <gedeon.jirehl.gangoue23@gmail.com>
2026-06-27 21:24:26 +0100

    login

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { useState } from 'react';
+import { motion } from 'motion/react';
+import { FiArrowRight } from 'react-icons/fi';
+import Link from 'next/link';
+import { AuthLayout, OAuthButtons, FormField, Spinner } from '../../../src/components/auth';
